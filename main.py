082f2463574ccc34f7deee36bfab252c3146a4c0
Luiz Henrique Teixeira Mendes <luizhteixeiram@gmail.com>
2020-04-22 17:11:01 -0300

    Algumas melhorias de velocidade, tirei boa parte dos sleeps manuais e agora estou usando o implicitly_wait() do driver. Ainda está dando alguns problemas em alguns casos

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import urllib.parse
-import time
 import math
+import sys
+import time
 from selenium import webdriver
 from get_ruas import pega_ruas
 from importa_csv import limpa_telefones_e_importa_para_csv
@@ -60,11 +61,12 @@ else:
         cidade = input("Digite o nome da cidade: ").strip()
 
 opts = webdriver.chrome.options.Options()
-opts.headless = True
+#opts.headless = True
 opts.add_argument('log-level=3') # so mostra erros graves no terminal
 
 url = "https://meuvivofixo.vivo.com.br/servlet/Satellite?c=Page&cid=1382552299186&pagename=MeuVivoFixo%2FPage%2FTemplateGlobalAreaAberta"
 driver = webdriver.Chrome(options=opts, executable_path=chromedriver_location)
+driver.implicitly_wait(10)
 driver.get(url)
 
 numero_inicial = 1
@@ -89,25 +91,25 @@ for street in street_list:
 
     xpath_quantidade_resultados = '//*[@id="formWCSVivo"]/div/p'
 
-    tentativas_maxima = 90
+    tentativas_maxima = 10
     for i in range(tentativas_maxima):
         try:
             texto_quantidade = driver.find_element_by_xpath(xpath_quantidade_resultados).text
         except:
-            time.sleep(4)
+            time.sleep(0.2)
             try:
                 xpath_botao_ok = '//*[@id="btnOK"]/span'
                 driver.find_element_by_xpath(xpath_botao_ok).click()
             except:
                 continue
             else:
-                time.sleep(1)
                 driver.find_element_by_xpath(xpath_botao_pesquisar).click()
-                time.sleep(1)
                 break
         else:
             break
 
+    time.sleep(0.2)
+    
     texto_quantidade = texto_quantidade.strip()
 
     if(texto_quantidade == "Nenhum número de telefone foi encontrado. Tente novamente!" or \
@@ -133,32 +135,24 @@ for street in street_list:
         for j in range(1, 16):
             xpath_tel = '//*[@id="formWCSVivo"]/table/tbody/tr[{j}]/td/table/tbody/tr[2]/td[1]'.format(j=j)
             xpath_nome = '//*[@id="formWCSVivo"]/table/tbody/tr[{j}]/td/table/tbody/tr[1]/td[1]/strong'.format(j=j)
-            tentativas = 5
-            for i in range(tentativas):
-                try:
-                    tel = driver.find_element_by_xpath(xpath_tel).text
-                    nome = driver.find_element_by_xpath(xpath_nome).text
-                except:
-                    time.sleep(3)
-                    continue
-                else:
-                    break
+            tel = driver.find_element_by_xpath(xpath_tel).text
+            nome = driver.find_element_by_xpath(xpath_nome).text
             file_telefones.write(tel + '\n')
             file_nomes.write(nome + '\n')
         xpath_botao_prox = '//*[@id="formWCSVivo"]/table/tbody/tr[16]/td/a[{i}]'.format(i=i+1)
         driver.find_element_by_xpath(xpath_botao_prox).click()
-        time.sleep(1)
-
-    restantes = quantidade_resultados % 15
-
-    if(restantes == 0):
-        restantes = 15
     
+    restantes = quantidade_resultados % 15
+    if restantes == 0:
+        restantes = 15
+
     for i in range(1, restantes + 1):
         xpath_tel = '//*[@id="formWCSVivo"]/table/tbody/tr[{i}]/td/table/tbody/tr[2]/td[1]'.format(i=i)
         xpath_nome = '//*[@id="formWCSVivo"]/table/tbody/tr[{i}]/td/table/tbody/tr[1]/td[1]/strong'.format(i=i)
+
         tel = driver.find_element_by_xpath(xpath_tel).text
         nome = driver.find_element_by_xpath(xpath_nome).text
+
         file_telefones.write(tel + '\n')
         file_nomes.write(nome + '\n')
 
@@ -177,8 +171,6 @@ for street in street_list:
 
     xpath_botao_nova_busca = '//*[@id="btnPesquisar"]/img'
     driver.find_element_by_xpath(xpath_botao_nova_busca).click()
-
-    time.sleep(3)
 
 file_telefones.close()
 file_nomes.close()
