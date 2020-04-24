9b84a2eabe38e249a6ba501234b2e9ec3cca5b9b
Luiz Henrique Teixeira Mendes <luizhteixeiram@gmail.com>
2020-04-23 22:36:50 -0300

    pegando ruas de uma nova fonte, aumentou consideravelmente o numero de ruas e consequentemente de telefones

--- get_ruas.py
+++ get_ruas.py
@@ -6,20 +6,28 @@ from bs4 import BeautifulSoup
 def pega_ruas(cidade):
     # limpa o parametro
     cidade = cidade.strip().lower()
-    cidade = cidade.replace(' ', '-')
-
-    url = "https://www.rastreamentocorreios.net/qual_cep/sp/{cidade}".format(cidade=cidade)
-    response = requests.get(url)
-
-    print(response)
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    table = soup.table
-    streets_table = table.findAll('a')
+    cidade = cidade.replace(' ', '+')
 
     lista_ruas = []
-    for element in streets_table:
-        street = element.get_text()
-        lista_ruas.append(street)
+    maximo_paginas = 1000
+    for i in range(1, maximo_paginas):
+        url = "https://cep.guiamais.com.br/busca/{cidade}-sp?page={i}".format(cidade=cidade, i=i)
+        response = requests.get(url)
+
+        if response.status_code == 404:
+            break
+
+        doc = BeautifulSoup(response.text, "html.parser")
+        table = doc.table
+        a_table = table.findAll('a')
+
+        for i in range(0, len(a_table), 5):
+            rua = a_table[i].text.strip()
+            lista_ruas.append(rua)
+
+        time.sleep(0.2)
     
-    return lista_ruas
+    sem_rep = list(set(lista_ruas))
+    sem_rep_filtrado = [string for string in sem_rep if string != ""]
+
+    return sem_rep_filtrado
